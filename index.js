2da33501ee65292d6cf1e34bee2c08370eb31ce2
George Hett <georgehett95@gmail.com>
2022-04-04 16:44:33 +0100

    Connected index to model
    Co-authored-by: Valentina Maggio <valentina-maggio@users.noreply.github.com>
    Co-authored-by: Samuel Raducan <samuelmbp@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Notes app is working!")
+const NotesModel = require('./notesModel.js');
+
+console.log("Notes app is working!");
+
+const notes = new NotesModel();
+// notes.addNote("Go to sleep")
+console.log(notes.getNotes());
