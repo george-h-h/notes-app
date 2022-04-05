ddfec35406c265894542a850c0ae197d8e1cb2a7
George Hett <georgehett95@gmail.com>
2022-04-05 16:07:37 +0100

    Added notes to the webpage
    Co-authored-by: Samuel Raducan <samuelmbp@users.noreply.github.com>
    Co-authored-by: Valentina Maggio <valentina-maggio@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const NotesModel = require("./notesModel");
 const NotesView = require("./notesView");
-console.log("Notes app is working!");
+// console.log("Notes app is working!");
 
 const notesModel = new NotesModel();
 const notesView = new NotesView(notesModel);
-notesModel.addNote("Go to sleep")
-console.log(notesView.displayNotes());
+
+notesModel.addNote("Go to sleep");
+notesModel.addNote("Stop working so hard");
+
+notesView.displayNotes();
