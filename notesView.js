935ea7237fbbe58757f5b007135638191ce04410
Valentina <maggio.vale@gmail.com>
2022-04-06 14:47:28 +0100

    Added remove method to clear previous notes
    Co-authored-by: Samuel Raducan <samuelmbp@users.noreply.github.com>

--- notesView.js
+++ notesView.js
@@ -4,8 +4,8 @@ class NotesView {
   constructor(notesModel) {
     this.notesModel = notesModel;
     this.mainContainer = document.querySelector("#main-container");
-    this.buttonEl = document.querySelector(".note");
-    this.input = document.querySelector("#add-note");
+    this.buttonEl = document.querySelector(".add-note");
+    this.input = document.querySelector("#note");
     this.buttonEl.addEventListener('click', () => {
       this.notesModel.addNote(this.input.value);
       this.displayNotes();
@@ -13,11 +13,13 @@ class NotesView {
   }
 
   displayNotes() {
-    const notes = this.notesModel.getNotes();
+    document.querySelectorAll('.new-note').forEach(el => el.remove());
 
+    const notes = this.notesModel.getNotes();
+    
     notes.forEach((note) => {
       let div = document.createElement("div");
-      div.classList.add("note");
+      div.classList.add("new-note");
       div.innerText = note;
       this.mainContainer.append(div);
     });
--- notesView.test.js
+++ notesView.test.js
@@ -12,13 +12,28 @@ describe("Notes View", () => {
     const model = new NotesModel();
     const view = new NotesView(model);
 
-    const buttonEl = document.querySelector(".note");
-    const inputEl = document.querySelector("#add-note");
+    const buttonEl = document.querySelector(".add-note");
+    const inputEl = document.querySelector("#note");
 
     inputEl.value = "Learning DOM";
     buttonEl.click();
 
-    expect(document.querySelectorAll("div.note").length).toBe(1);
-    expect(document.querySelectorAll("div.note")[0].innerText).toEqual("Learning DOM");
+    expect(document.querySelectorAll("div.new-note").length).toBe(1);
+    expect(document.querySelectorAll("div.new-note")[0].innerText).toEqual("Learning DOM");
+  });
+
+  it('clears the previous notes', () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    const buttonEl = document.querySelector(".add-note");
+    const inputEl = document.querySelector("#note");
+
+    inputEl.value = "Learning DOM";
+    buttonEl.click();
+    buttonEl.click();
+
+    expect(document.querySelectorAll("div.new-note").length).toEqual(2);
   });
 });
